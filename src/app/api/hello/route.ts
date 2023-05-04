bf24a2371781c70c51e82f8a2ef00d3c6e3368f6
LuizArnoldChavezBurgos <luiz.chavez@unmsm.edu.pe>
2023-05-04 17:39:24 -0500

    first commit

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,17 @@
+import prisma from '@/../prisma/client';
+
+export async function GET(request: Request) {
+
+  try {
+    await prisma.user.create({
+      data: {
+        email: 'elso@prisma.io',
+        name: 'Elso Prisma',
+      }
+    })
+  } catch (error) {
+    return new Response("email already exists")
+  }
+
+  return new Response(`Hello, Next.js!`)
+}
